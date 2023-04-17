4525a373fb7373fe906adc3dc405ce99e797829f
Wilkson Flor <wilksonflor12@gmail.com>
2023-04-16 21:29:47 -0300

    Tentando importar o css e alterar o HBS

--- src/index.js
+++ src/index.js
@@ -1,53 +1,49 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const path = require('path');
-const hbs= require('hbs')
-const collection = require('./mongodb')
+const path = require("path");
+const hbs = require("hbs");
+const collection = require("./mongodb");
 
-const templatePath = path.join(__dirname, '../templates')
-app.use(express.json())
-app.set("view engine", "hbs")
-app.set('views', templatePath)
-app.use(express.urlencoded({extended:false}))
+const templatePath = path.join(__dirname, "../templates");
+app.use(express.json());
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("login")
-})
+  res.render("login");
+});
 
 app.get("/signup", (req, res) => {
-    res.render("signup")
-})
+  res.render("signup");
+});
 
-app.post("/signup", async (req,res) =>{
-    const data = {
-        name: req.body.name,
-        password: req.body.password
+app.post("/signup", async (req, res) => {
+  const data = {
+    name: req.body.name,
+    password: req.body.password,
+  };
+
+  await collection.insertMany([data]);
+
+  res.render("home");
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const check = await collection.findOne({ name: req.body.name });
+    if (check.password === req.body.password) {
+      res.render("home");
+    } else {
+      res.send("Wrong Password");
     }
+  } catch {
+    res.send("Wrong details");
+  }
+});
 
-    
-    await collection.insertMany([data]);
-
-    res.render("home");
-})
-
-app.post("/login", async (req,res) =>{
-    
-
-   try{
-    const check = await collection.findOne({name:req.body.name});
-        if(check.password=== req.body.password ){
-            res.render("home")
-        }
-        else{
-            res.send("Wrong Password")
-        }
-   }
-   catch{
-    res.send("Wrong details")
-   }
-})
-
-
-app.listen(3000, () =>{
-    console.log('Connected');
-})
+app.listen(3000, () => {
+  console.log("Connected");
+});
